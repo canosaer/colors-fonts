03f22dfc9c8997c56f84000927b3a371e348ce10
canosaer <67963828+canosaer@users.noreply.github.com>
2022-04-17 12:00:33 -0400

    fonts

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,31 @@
-import React, {useState, useContext, useEffect} from 'react';
-import {Context} from '../store/store'
+import React, { useState } from 'react';
+import fonts from '../store/fonts'
 
 export default function Filters() {
-    const [state, dispatch] = useContext(Context)
 
-    const updateFilters = (e) => {
+    const [ fontPair, setFontPair ] = useState(0);
 
-        // dispatch ({type: 'UPDATE_TAGS', payload: updatedTags})
+    const updateFilters = (value) => {
+        const cssRootVariables = document.documentElement.style
+        const headingFont = `${fonts[value][0].name}, ${fonts[value][0].fallback}`
+        const textFont = `${fonts[value][1].name}, ${fonts[value][1].fallback}`
+
+        cssRootVariables.setProperty('--heading-font', headingFont)
+        cssRootVariables.setProperty('--text-font', textFont)
     }
 
 
     return(
         <div className="filter-row">
-            <select name="fonts" className="dropdown-filters" id="fonts" onChange={(e) => updateFilters(e)}>
-                <option value="all">All</option>
+            <select name="fonts" className="dropdown-filters" id="fonts" onChange={(e) => updateFilters(e.target.value)}>
+                {fonts.map((pair, i) => {
+                    const key = `pair--${i}`
+                    const pairDisplay = `${pair[0].name} / ${pair[1].name}`
 
+                    return(
+                        <option key={key} className="dropdown-filters__option" value={i}>{pairDisplay}</option>
+                    )
+                })}
             </select>
         </div>
 
