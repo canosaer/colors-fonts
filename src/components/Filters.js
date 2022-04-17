ff036f083bb6a2facf361c10d3740f17bac7a124
canosaer <67963828+canosaer@users.noreply.github.com>
2022-04-17 17:23:24 -0400

    1.0

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -12,12 +12,61 @@ export default function Filters() {
 
         cssRootVariables.setProperty('--heading-font', headingFont)
         cssRootVariables.setProperty('--text-font', textFont)
+        
+        if( value === 2 ) cssRootVariables.setProperty('--text-transform', 'uppercase')
+        else cssRootVariables.setProperty('--text-transform', 'capitalize')
+
+        if( value === 0 ){
+            cssRootVariables.setProperty('--menu-font-size', '1.1rem')
+            cssRootVariables.setProperty('--bar-spacing-mobile', '1.55rem')
+            cssRootVariables.setProperty('--bar-spacing-desktop', '1.3rem')
+            cssRootVariables.setProperty('--menu-font-mobile', '1.25rem')
+            cssRootVariables.setProperty('--heading-font-small', '2rem')
+            cssRootVariables.setProperty('--heading-font-big', '2.75rem')
+        } 
+        else if( value === 1 ){
+            cssRootVariables.setProperty('--menu-font-size', '0.95rem')
+            cssRootVariables.setProperty('--bar-spacing-mobile', '1.6rem')
+            cssRootVariables.setProperty('--bar-spacing-desktop', '1.85rem')
+            cssRootVariables.setProperty('--menu-font-mobile', '1rem')
+            cssRootVariables.setProperty('--heading-font-small', '1.75rem')
+            cssRootVariables.setProperty('--heading-font-big', '2.3rem')
+            
+        } 
+        else if( value === 2 ){
+            cssRootVariables.setProperty('--menu-font-size', '1.85rem')
+            cssRootVariables.setProperty('--bar-spacing-mobile', '1.6rem')
+            cssRootVariables.setProperty('--bar-spacing-desktop', '1.85rem')
+            cssRootVariables.setProperty('--menu-font-mobile', '1.25rem')
+            cssRootVariables.setProperty('--heading-font-small', '2.4rem')
+            cssRootVariables.setProperty('--heading-font-big', '3rem')
+            
+        }
+        else if( value === 3 ){
+            cssRootVariables.setProperty('--menu-font-size', '1.4rem')
+            cssRootVariables.setProperty('--bar-spacing-mobile', '1.5rem')
+            cssRootVariables.setProperty('--bar-spacing-desktop', '1.6rem')
+            cssRootVariables.setProperty('--menu-font-mobile', '1.25rem')
+            cssRootVariables.setProperty('--heading-font-small', '2.4rem')
+            cssRootVariables.setProperty('--heading-font-big', '3rem')
+            
+        } 
+        else{
+            cssRootVariables.setProperty('--menu-font-size', '1rem')
+            cssRootVariables.setProperty('--bar-spacing-mobile', '1.55rem')
+            cssRootVariables.setProperty('--bar-spacing-desktop', '1.3rem')
+            cssRootVariables.setProperty('--menu-font-mobile', '1.25rem')
+            cssRootVariables.setProperty('--heading-font-small', '2rem')
+            cssRootVariables.setProperty('--heading-font-big', '2.75rem')
+        }
+        
+
     }
 
 
     return(
         <div className="filter-row">
-            <select name="fonts" className="dropdown-filters" id="fonts" onChange={(e) => updateFilters(e.target.value)}>
+            <select name="fonts" className="dropdown-filters" id="fonts" onChange={(e) => updateFilters(parseInt(e.target.value))}>
                 {fonts.map((pair, i) => {
                     const key = `pair--${i}`
                     const pairDisplay = `${pair[0].name} / ${pair[1].name}`
