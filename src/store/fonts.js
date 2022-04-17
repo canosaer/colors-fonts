e8ef62e8f3f36f56d5faf4244841d67e766c8e16
canosaer <67963828+canosaer@users.noreply.github.com>
2022-04-16 21:38:31 -0400

    fonts

---
+++ src/store/fonts.js
@@ -0,0 +1,48 @@
+const fonts = [
+    [   
+        {   name: 'Audiowide',
+            fallback: 'cursive',
+        },
+        {   name: 'Mukta Vaani',
+            fallback: 'sans-serif',
+        },
+    ],
+    [   
+        {   name: 'Sirin Stencil',
+            fallback: 'cursive',
+        },
+        {   name: 'Varela',
+            fallback: 'sans-serif',
+        },
+    ],
+    [   
+        {   name: 'Black Ops One',
+            fallback: 'cursive',
+        },
+        {   name: 'Halant',
+            fallback: 'serif',
+        },
+    ],
+    [   
+        {   name: 'Baumans',
+            fallback: 'cursive',
+        },
+        {   name: 'Martel',
+            fallback: 'serif',
+        },
+    ],
+    [   
+        {   name: 'Revalia',
+            fallback: 'cursive',
+        },
+        {   name: 'Cairo',
+            fallback: 'sans-serif',
+        },
+    ],
+    
+    
+
+    
+]
+
+export default fonts
