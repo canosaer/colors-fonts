a1cba3ac078e0bbedc1d4139a04df0c06d637db5
canosaer <canosaer@gmail.com>
2022-04-25 18:41:37 -0400

    cube 1.0

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,30 +1,82 @@
-import React from 'react';
+import React, { useEffect, useRef, useState} from 'react';
 import Cube from 'react-3d-cube';
+import { gsap } from "gsap";
 
 const LogoCube = () => {
+
+    const [ cubeWrapper, setCubeWrapper ] = useState(null);
+    const [ cubeInitialized, setCubeInitialized ] = useState(false);
+
+    const containerDiv = useRef();
+
+    useEffect(() => {
+        if(cubeWrapper && !cubeInitialized){
+            cubeWrapper.classList.add('enter-transition')
+            setCubeInitialized(true)
+            cubeWrapper.classList.add('initial-cube')
+
+            const timer = setTimeout(() => {
+                cubeWrapper.classList.remove('enter-transition')
+                cubeWrapper.classList.add('exit-transition')
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [cubeWrapper]);
+
+    const unlockCube = () => {
+        cubeWrapper.classList.remove('initial-cube')
+        const timer = setTimeout(() => {
+            if(cubeWrapper.classList.contains('exit-transition')) cubeWrapper.classList.remove('exit-transition')
+            if(cubeWrapper.classList.contains('enter-transition')) cubeWrapper.classList.remove('enter-transition')
+        }, 1500);
+        return () => clearTimeout(timer);
+    }
+
+
+    useEffect(() => {
+        if(containerDiv.current){
+            setCubeWrapper(containerDiv.current.firstChild.firstChild)
+        }
+    }, [containerDiv]);
 
     return (
       <div>
         <center>
-            <h1>react-3d-cube</h1>
-            <h2>set children</h2>
-            <div
+            <div ref={containerDiv} onMouseEnter={unlockCube}
             style={{
-                width: 300,
-                height: 300
+                width: 200,
+                height: 200,
             }}
             >
-            <Cube size={300} index="front">
-                <div className="cube__front">E</div>
-                <div className="cube__right">C</div>
-                <div className="cube__back">E</div>
-                <div className="cube__left">C</div>
-                <div className="cube__top"></div>
-                <div className="cube__bottom"></div>
-            </Cube>
+                <Cube size={200} index="front">
+                    <div className="cube__content"><p className="cube__letter">E</p></div>
+                    <div className="cube__content"><p className="cube__letter">C</p></div>
+                    <div className="cube__content"><p className="cube__letter">E</p></div>
+                    <div className="cube__content"><p className="cube__letter">C</p></div>
+                    <div className="cube__content">
+                        <div className="cube__logo">
+                            <div className="e e_audiowide e_dark"> E
+                                <div className="e__curve-up e__curve-up_dark"></div>
+                                <div className="e__curve-down e__curve-down_dark"></div>
+                                <div className="e__curve-across e__curve-across_dark"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cube__content">
+                        <div className="cube__logo">
+                            <div className="e e_audiowide e_dark"> E
+                                <div className="e__curve-up e__curve-up_dark"></div>
+                                <div className="e__curve-down e__curve-down_dark"></div>
+                                <div className="e__curve-across e__curve-across_dark"></div>
+                            </div>
+                        </div>
+                    </div>
+                </Cube>
             </div>
         </center>
       </div>
     );
 }
+
 export default LogoCube
--- src/components/Logos.js
+++ src/components/Logos.js
@@ -7,32 +7,32 @@ export default function Logos() {
         <section className="logos" id="logos">
             <h3 className="logos__heading">Logos</h3>
             <div className="logos__card-display">
-                <article className="logos__card logo__card">
+                <article className="logos__card">
                     <div className="cube">
                         <LogoCube />
                     </div>
                 </article>
-                <article className="logos__card logo__card">
+                <article className="logos__card">
                     <div className="e e_audiowide e_dark"> E
                         <div className="e__curve-up e__curve-up_dark"></div>
                         <div className="e__curve-down e__curve-down_dark"></div>
                         <div className="e__curve-across e__curve-across_dark"></div>
                     </div>
                 </article>
-                <article className="logos__card logo__card">
+                <article className="logos__card">
                     <div className="e e_audiowide e_light"> E
                         <div className="e__curve-up e__curve-up_light"></div>
                         <div className="e__curve-down e__curve-down_light"></div>
                         <div className="e__curve-across e__curve-across_light"></div>
                     </div>
                 </article>
-                <article className="logos__card logo__card">
+                <article className="logos__card">
                     <div className="e e_audiowide e_gradient"> E </div>
                 </article>
-                <article className="logos__card logo__card">
+                <article className="logos__card">
                     <div className="e e_audiowide e_light"> E </div>
                 </article>
-                <article className="logos__card logo__card">
+                <article className="logos__card">
                     <div className="e e_audiowide e_dark"> E </div>
                 </article>
             </div>
