940c6acae53a8a8848b4aad1c108b9e6607d8195
canosaer <canosaer@gmail.com>
2022-04-25 14:13:42 -0400

    styling

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -7,15 +7,6 @@ const LogoCube = () => {
       <div>
         <center>
             <h1>react-3d-cube</h1>
-            <h2>no children</h2>
-            <div
-            style={{
-                width: 300,
-                height: 300
-            }}
-            >
-            <Cube size={300} index="front" />
-            </div>
             <h2>set children</h2>
             <div
             style={{
@@ -24,12 +15,12 @@ const LogoCube = () => {
             }}
             >
             <Cube size={300} index="front">
-                <div>front</div>
-                <div>right</div>
-                <div>back</div>
-                <div>left</div>
-                <div>top</div>
-                <div>bottom</div>
+                <div className="cube__front">E</div>
+                <div className="cube__right">C</div>
+                <div className="cube__back">E</div>
+                <div className="cube__left">C</div>
+                <div className="cube__top"></div>
+                <div className="cube__bottom"></div>
             </Cube>
             </div>
         </center>
